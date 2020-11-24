25ef0cc4e59c839cd47731e4b8713320721075ea
adrianyaniri <agyaniri@gmail.com>
2020-11-23 23:00:50 -0300

    menu de opciones

---
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Menu.kt
@@ -0,0 +1,10 @@
+package ar.edu.unahur.obj2.impostoresPaises
+
+import java.awt.Choice
+
+object Menu {
+    fun elegirOpcion() {
+
+    }
+
+}
--- src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Programa.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/impostoresPaises/Programa.kt
@@ -2,7 +2,7 @@ package ar.edu.unahur.obj2.impostoresPaises
 
 // Acá encapsulamos el manejo de la consola real, desacoplandolo del programa en sí
 object Consola {
-  fun leerLinea() = readLine()
+  fun leerLinea() = readLine()!!
   fun escribirLinea(contenido: String) {
     println(contenido)
   }
@@ -12,17 +12,21 @@ object Consola {
 object Programa {
   // Ambas son variables para poder cambiarlas en los tests
   var entradaSalida = Consola
-  var api = RestCountriesAPI()
+
 
   fun iniciar() {
-
-    entradaSalida.escribirLinea("Hola, poné el nombre de un país y te mostramos algo de data")
+    entradaSalida.escribirLinea("elije un pais")
     val pais = entradaSalida.leerLinea()
-    checkNotNull(pais) { "Sin nombre no puedo hacer nada :(" }
-    val paisesEncontrados = api.buscarPaisesPorNombre(pais)
-    check(paisesEncontrados.isNotEmpty())
+    entradaSalida.escribirLinea("elije otro pais")
+    val pais2 = entradaSalida.leerLinea()
+
+    comparSiSonLimitrofes(pais, pais2)
+
+
+    /*val paisesEncontrados1 = api.buscarPaisesPorNombre(pais)
+    check(paisesEncontrados1.isNotEmpty())
       { "No encontramos nada, fijate si lo escribiste bien" }
-    paisesEncontrados.forEach {
+    paisesEncontrados1.forEach {
       entradaSalida.escribirLinea(
         "${it.name} (${it.alpha3Code}) es un país de ${it.region}, con una población de ${it.population} habitantes."
       )
@@ -31,24 +35,17 @@ object Programa {
     entradaSalida.escribirLinea("Hola, poné el nombre de un país y te mostramos algo de data")
     val otroPais = entradaSalida.leerLinea()
     checkNotNull(pais) { "Sin nombre no puedo hacer nada :(" }
+
     val paiseAComparar = otroPais?.let { api.buscarPaisesPorNombre(it) }
     if (paiseAComparar != null) {
       check(paiseAComparar.isNotEmpty())
       { "No encontramos nada, fijate si lo escribiste bien" }
-    }
-
-
-
-
+    }*/
 
   }
-
-
-
-
-
-
-
+  fun comparSiSonLimitrofes(pais:String, pais2:String) =
+    if (Observatorio.sonLimitrofes(pais,pais2)) entradaSalida.escribirLinea("los paises ${pais}, ${pais2} son limitrofes")
+    else entradaSalida.escribirLinea("los paises ${pais},${pais2} no limitan")
 
 
 }
