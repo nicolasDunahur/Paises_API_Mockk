f9854356c641a2b9dd8edbfc4a4e75244608482a
Nicolas D <profesionalnicolasdiaz@gmail.com>
2020-11-25 01:27:07 -0300

    corre sin internet

--- src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/ProgramaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/impostoresPaises/ProgramaTest.kt
@@ -1,13 +1,26 @@
 package ar.edu.unahur.obj2.impostoresPaises
 
+import ar.edu.unahur.obj2.impostoresPaises.Observatorio.api
 import io.kotest.core.spec.style.DescribeSpec
 import io.mockk.*
+import kotlin.collections.mutableListOf as mutableListOf1
 
 
 class ProgramaTest : DescribeSpec({
   describe("Programa") {
     val consolaMock = mockk<Consola>()
     Programa.entradaSalida = consolaMock
+    Observatorio.api = mockk<RestCountriesAPI>()
+
+    val espaniol = Language("espaniol")
+    val ingles = Language("ingles")
+    val MERCOSUR = RegionalBloc("MERCOSUR","MERCOSUER")
+
+    every { api.buscarPaisesPorNombre("Argentina") } returns listOf(
+            Country("Argentina", "ARG", "BS AS", "America", 43, kotlin.collections.mutableListOf("CHL"), kotlin.collections.mutableListOf(espaniol), kotlin.collections.mutableListOf(MERCOSUR)))
+
+    every { api.buscarPaisesPorNombre("Russia") } returns listOf(
+            Country("Russia", "RUS", "Moscow", "Asia", 100, kotlin.collections.mutableListOf("MON"), kotlin.collections.mutableListOf(), kotlin.collections.mutableListOf()))
 
     it("1. Paises limitrofes") {
       every { consolaMock.escribirLinea(any()) } just Runs
@@ -45,6 +58,13 @@ class ProgramaTest : DescribeSpec({
     it("4. Paises con mayor poblacion") {
       every { consolaMock.escribirLinea(any()) } just Runs
       every { consolaMock.leerLinea() } returns "4"
+      every { api.todosLosPaises() } returns listOf(
+              Country("United States of America", "USA", "Washington", "America", 3, mutableListOf1("CAN", "MEX"), mutableListOf1(ingles), mutableListOf1()),
+              Country("Brazil", "BRA", "Brasilia", "America", 1, mutableListOf1("ARG", "URU"), mutableListOf1(espaniol), mutableListOf1(MERCOSUR)),
+              Country ("China", "", "", "", 5, mutableListOf1("BRA"), mutableListOf1(), mutableListOf1()),
+              Country("India", "", "", "", 4, mutableListOf1("BRA"), mutableListOf1(), mutableListOf1()),
+              Country("Indonesia", "", "", "", 2, mutableListOf1("BRA"), mutableListOf1(), mutableListOf1())
+      )
 
       Programa.iniciar()
 
@@ -54,6 +74,7 @@ class ProgramaTest : DescribeSpec({
     }
 
     it("5. Continente con mayor poblacion") {
+      every { api.todosLosPaises() } returns listOf(Country("China", "", "", "Asia", 5, mutableListOf1("CHI"), mutableListOf1(), mutableListOf1()), )
       every { consolaMock.escribirLinea(any()) } just Runs
       every { consolaMock.leerLinea() } returns "5"
 
